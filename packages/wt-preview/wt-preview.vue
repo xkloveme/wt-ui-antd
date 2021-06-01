3f7999142d75bba9559d8296942e396cb9a82562
yanzhe <1013951740@qq.com>
2021-06-01 15:21:35 +0800

    feat: add wt-preview

---
+++ packages/wt-preview/wt-preview.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+    <div class=""></div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    watch: {},
+    //方法集合
+    methods: {},
+    mounted() {},
+    beforeCreate() {}, //生命周期 - 创建之前
+    beforeMount() {}, //生命周期 - 挂载之前
+    beforeUpdate() {}, //生命周期 - 更新之前
+    updated() {}, //生命周期 - 更新之后
+    beforeDestroy() {}, //生命周期 - 销毁之前
+    destroyed() {}, //生命周期 - 销毁完成
+    activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+<style lang="css" scoped></style>
